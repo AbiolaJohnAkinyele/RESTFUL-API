6efd80005036cc65b7f2bf6f93bc5a5f64f0c390
Abiola John Akinyele <105801057+AbiolaJohnAkinyele@users.noreply.github.com>
2025-05-19 18:21:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const errorHandler = require('./middleware/errorHandler');
 
 app.use(express.json());
 
-// Root route
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Routes
+
 app.use('/items', itemsRouter);
 
-// 404 handler
+
 app.use((req, res, next) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
-// Global error handler
+
 app.use(errorHandler);
 
 const PORT = 3000;
